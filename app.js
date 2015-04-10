409a5bbc95ad94b642ce9fb4e03ef82a9a132dfa
Karthik Balakrishnan <karthikb351@gmail.com>
2015-04-11 00:55:55 +0530

    Serve src directly.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 5532);
