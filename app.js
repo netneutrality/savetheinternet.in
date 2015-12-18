dab8c375686cef4081d429b51649e4f7aabaa120
Mitesh Ashar <email@miteshashar.com>
2015-12-18 13:39:18 +0530

    Removed blog redirection

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function (req, res) {
-	res.redirect(307, "http://blog.savetheinternet.in");
-});
 app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 5532);
