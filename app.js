22f392cd0fc188b8e57a3e453aba1721a2cbefd9
Mitesh Ashar <email@miteshashar.com>
2016-01-21 14:09:02 +0530

    Moved old stuff to archived folder. Redirection for lately circulated urls

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var app = express();
 
+var go_to_root = function(req, res){
+	res.redirect(302, '/');
+};
+
+app.get('/mp', go_to_root);
+app.get('/hi', go_to_root);
+app.get('/hi/mp', go_to_root);
+app.get('/fb-trai', go_to_root);
+
 app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 5532);
